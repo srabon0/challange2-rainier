a404637078a98b8cb7205c969296ef59f2758727
srabon0 <srabonemam4@gmail.com>
2022-06-26 11:45:14 +0600

    just menu created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FaArrowCircleLeft, FaBars } from "react-icons/fa";
+import { IoIosArrowBack , IoIosMenu} from "react-icons/io";
 const Navbar = () => {
+  const [open, setOpen] = useState(true);
   return (
-    <div class="drawer px-4">
-      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col">
-        <div class="w-full navbar bg-base-100">
-          <div class="flex-none lg:hidden">
-            <label for="my-drawer-3" class="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div class="flex-1 px-2 mx-2">Home</div>
+    <div className="flex">
+      <div
+        className={`bg-base-100 shadow-sm h-screen p-5 pt-8 relative duration-300 ${
+          open ? "w-64" : "w-20"
+        } cursor-pointer`}
+      >
 
-          <div class="flex-none block">
-            <ul class="menu menu-horizontal">
-              <li>
-                <a> <button className="btn btn-primary" >jhh</button> </a>
-              </li>
-              <li tabindex="0">
-                <a>
-                <div class="avatar">
-                <div class="w-10 rounded-full">
-                  <img src="https://api.lorem.space/image/face?hash=92310" />
-                </div>
-              </div>
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-             
-              
-            </ul>
-          </div>
-        </div>
-        Content
+
+       {
+        open ? "Hello" :  <IoIosMenu
+        className="text-primary bg-gray-100 text-4xl shadow-lg rounded font-semibold text-2xl"
+        onClick={() => setOpen(true)}
+      ></IoIosMenu>
+       }
+        
+        <IoIosArrowBack
+          className="text-primary rounded-full text-4xl p-2 absolute border shadow-lg -right-5 top-8 bg-white"
+          onClick={() => setOpen(false)}
+        ></IoIosArrowBack>
       </div>
-      <div class="drawer-side">
-        <label for="my-drawer-3" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+
+      <div className="p-7">
+        <h1 className="text-2xl font-semibold">HomePage</h1>
       </div>
     </div>
   );
